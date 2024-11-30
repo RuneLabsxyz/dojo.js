05b62d2d045150c76091dcd399259785578acaee
0xmugen <pierre.semanne@gmail.com>
2024-11-30 14:29:26 +0100

    feat: attempt to init the sdk

--- examples/example-vite-svelte-sdk/src/routes/+page.svelte
+++ examples/example-vite-svelte-sdk/src/routes/+page.svelte
@@ -1,2 +1,34 @@
+<script lang="ts">
+    import { init } from "@dojoengine/svelte-sdk";
+    import { Schema, schema } from "$lib/bindings";
+    import { dojoConfig } from "$lib/dojoConfig";
+    import { onMount } from "svelte";
+    import { writable } from 'svelte/store';
+
+    const sdk = writable();
+
+    onMount(async () => {
+        const initialized = await init<Schema>(
+            {
+                client: {
+                    rpcUrl: dojoConfig.rpcUrl,
+                    toriiUrl: dojoConfig.toriiUrl,
+                    relayUrl: dojoConfig.relayUrl,
+                    worldAddress: dojoConfig.manifest.world.address,
+                },
+                domain: {
+                    name: "WORLD_NAME",
+                    version: "1.0",
+                    chainId: "KATANA",
+                    revision: "1",
+                },
+            },
+            schema
+        );
+        sdk.set(initialized);
+    });
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
