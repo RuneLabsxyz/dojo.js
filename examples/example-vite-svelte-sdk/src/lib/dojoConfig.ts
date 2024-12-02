386f1184d7ff116d5456344565df6409c1e88935
0xmugen <pierre.semanne@gmail.com>
2024-12-02 12:17:53 +0100

    fix: set urls to 127.0.0.1

--- examples/example-vite-svelte-sdk/src/lib/dojoConfig.ts
+++ examples/example-vite-svelte-sdk/src/lib/dojoConfig.ts
@@ -4,4 +4,6 @@ import manifest from "../../../../worlds/dojo-starter/manifest_dev.json";
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    rpcUrl: "http://127.0.0.1:5050",
+    toriiUrl: "http://127.0.0.1:8080",
 });
